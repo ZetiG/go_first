73adbea3df4392e9d610d5f425cb1ff673c6a444
zeti <zmk95511@163.com>
2022-04-28 18:11:16 +0800

    upload post http server

--- my_function/test_01/Function_test_03.go
+++ my_function/test_01/Function_test_03.go
@@ -2,8 +2,10 @@ package test_01
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -31,7 +33,8 @@ type people interface {
 
 // 简单的http服务
 func httpTest() {
-	http.HandleFunc("/", sayHelloName)       //设置访问的路由
+	http.HandleFunc("/", sayHelloName) //设置访问的路由
+	http.HandleFunc("/upload", upload)
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
@@ -49,4 +52,32 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("val:", strings.Join(v, ""))
 	}
 	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+	if method != "POST" {
+		fmt.Fprintf(w, "request method exception")
+		return
+	}
+
+	r.ParseMultipartForm(32 << 20)
+	file, header, err := r.FormFile("file")
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	defer file.Close()
+	fmt.Fprintf(w, "%v", header)
+
+	f, err := os.OpenFile("/test_01/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+
 }
